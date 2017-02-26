130d646424cbd7f4794f9a0b8278a0a6be7e2f0a
xyuzuki <lindaliu@Yuzuki.local>
2017-02-26 15:22:47 -0500

    Got Rid of other method

--- Recursive.java
+++ Recursive.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
   
 public class Recursive
 {
+  static int count = 0;
   public static void main(String []args)
   {
     Scanner keyboard = new Scanner(System.in);
@@ -21,16 +22,13 @@ public class Recursive
     
     System.out.println(squareRoot(x, initialGuess, error));
     
-    
-    double newGuess = squareRoot(x, initialGuess, error);
-    
-    System.out.println(squareRoot(x, newGuess));
-    
   }
   
   public static double squareRoot(double x, double initialGuess, double error)
   {
-    double newGuess;
+    count++;
+    
+   
     if(Math.abs(initialGuess * initialGuess - x) <= error)
     {
       return initialGuess;
@@ -40,25 +38,12 @@ public class Recursive
       initialGuess = 0.5 * (initialGuess + (x / initialGuess));
       return initialGuess;
     }
-  }
+   
+   }
+    
+    
   
-  public static double squareRoot(double x, double newGuess)
-  {
-   double difference = Double.MAX_VALUE;
-   newGuess = x / 2;
-    double lastGuess = newGuess;
-    
-    
-    while(Math.abs(difference) >= 0.01)
-    {
-      double a = x / newGuess;
-      newGuess = (newGuess + a) / 2;
-      difference = ((newGuess - lastGuess) / lastGuess);
-      lastGuess = newGuess;
-      
-    }
-    return newGuess;
-  }
+  
  
 }
   
